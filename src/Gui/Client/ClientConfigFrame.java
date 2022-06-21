f6e7fd7a811c31051f2ce4e08f5002a197a9de12
José Gonçalves <jpedrogoncalves@ua.pt>
2022-06-21 15:34:13 +0100

    Fixing some more misnaming, a few bugs in the request tables and the client not receiving the reply

--- src/Gui/Client/ClientConfigFrame.java
+++ src/Gui/Client/ClientConfigFrame.java
@@ -10,7 +10,7 @@ import java.awt.*;
 public class ClientConfigFrame extends JFrame {
     private final JPanel mainPanel = new JPanel();
     private final LabeledTextFieldPanel ipField = new LabeledTextFieldPanel("Client IP: ", "localhost");
-    private final LabeledTextFieldPanel portField = new LabeledTextFieldPanel("Client Port: ", "99999");
+    private final LabeledTextFieldPanel portField = new LabeledTextFieldPanel("Client Port: ", "10000");
     private final LabeledTextFieldPanel idField = new LabeledTextFieldPanel("Client ID: ", "99");
     private IStartCallback callback = (ip, port, id) -> System.out.println(ip + " " + port + " " + id);
 
